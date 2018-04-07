722c59f55287291f3a1cb8ee425aab5e65a3ed98
Steven Maio <Stevenmaio.321@gmail.com>
2018-04-07 15:05:58 -0400

    Cleaned problem1 and removed executable

--- Project Euler/problem0XX/problem1/problem1.c
+++ Project Euler/problem0XX/problem1/problem1.c
@@ -2,16 +2,14 @@
 
 int main(void)
 {
-	int i, sum;
+	int i, sum = 0;
 
-	i = 0, sum = 0;
-
-	while (i < 1000)
+	for (i = 0; i < 1000; i ++)
 	{
-		if (i%3 && i%5);	// true if i isn't divisible by 3 or 5
+		if (i%3 && i%5);	// True if i isn't divisible by 3 or 5
 		else
 			sum += i;
-		i++;
 	}
 
-	printf("%d\n", sum); }
+	printf("%d\n", sum); 
+}
