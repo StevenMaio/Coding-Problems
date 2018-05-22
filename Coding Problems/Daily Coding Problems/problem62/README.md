4601a40004aad407462b2b05f1ecbdb212510dd7
Steven Maio <Stevenmaio.321@gmail.com>
2018-05-22 14:27:24 -0400

    Added problem 62

---
+++ Coding Problems/Daily Coding Problems/problem62/README.md
@@ -0,0 +1,9 @@
+# Problem 62
+There is an N by M matrix of zeroes. Given N and M, write a function to 
+count the number of ways of starting at the top-left corner and getting 
+to the bottom-right corner. You can only move right or down.
+
+### Using this program
+To run this program, type into the terminal `python problem62.py m n` where 
+`m` represents the number of rows in the matrix, and `n` represents the 
+number of columns.
---
+++ Coding Problems/Daily Coding Problems/problem62/problem62.py
@@ -0,0 +1,37 @@
+from sys import argv
+
+def num_paths(m, n):
+	matrix = []
+
+	for i in range(m):
+		row = []
+
+		for j in range(n):
+			row.append(0)
+
+		matrix.append(row)
+
+	# Set the last coordinate to 1
+	matrix[-1][-1] = 1
+
+	for i in range(m)[::-1]:
+
+		for j in range(n)[::-1]:
+			if i != m-1:
+				matrix[i][j] = matrix[i+1][j] + matrix[i][j]
+
+			if j != n-1:
+				matrix[i][j] = matrix[i][j+1] + matrix[i][j]
+
+	return matrix[0][0]
+			
+
+def main():
+	m = int(argv[1])
+	n = int(argv[2])
+
+	solution = num_paths(m, n)
+	print('{}'.format(solution))
+
+if __name__ == '__main__':
+	main()
