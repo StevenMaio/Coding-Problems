b7b678ca58a5788ee12c2e3c91f356102ab19ad6
Steven Maio <Stevenmaio.321@gmail.com>
2018-06-04 13:27:53 -0400

    Added Problem 78

---
+++ Coding Problems/Daily Coding Problems/problem78/README.md
@@ -0,0 +1,9 @@
+# Problem 78
+Given k sorted singly linked lists, write a function to merge all the lists 
+into one sorted singly linked list.
+
+## Solution
+TO use this solution, type into the terminal `python problem78.py 
+l_1 l_2 ... l_n` where each `l_i` corresponds to a list of integers formated
+like `a_1,a_2,...,a_n`. This isn't 100% to the problem, but the idea is the
+same.
---
+++ Coding Problems/Daily Coding Problems/problem78/problem78.py
@@ -0,0 +1,61 @@
+from sys import argv
+import pdb
+
+def merge(lists):
+	result = []
+	not_empty = False
+
+	# Untiil all of the lists are empty, we find whichever 
+	for x in lists:
+		if x:
+			not_empty = True
+
+	# Find the smallest first element and push it to result
+	# repeat until all of the lists are empty
+	while not_empty:
+		not_empty = False
+		min_element = None
+		min_list = None
+
+		for l in lists:
+			if l:
+				if min_element:
+					if l[0] < min_element:
+						min_element = l[0]
+						min_list = l
+
+				else:
+					min_element = l[0]
+					min_list = l
+
+		result.append(min_list.pop(0))
+
+		# Check to see if there are still empty elements
+		for l in lists:
+			if l:
+				not_empty = True
+				continue
+
+	return result
+
+def main():
+	lists = []
+
+	for arg in argv[1:]:
+		l = []
+
+		arg = arg.split(',')
+		
+		for x in arg:
+			l.append(int(x))
+
+		lists.append(l)
+
+	for x in lists:
+		x.sort()
+
+	solution = merge(lists)
+	print(solution)
+
+if __name__ == '__main__':
+	main()
