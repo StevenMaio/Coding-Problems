06cd1be7f29bfa9560a49d96ed92e01c81a694be
Steven Maio <Stevenmaio.321@gmail.com>
2018-06-18 21:43:45 -0400

    Made some changes to Bathroom Stalls

--- CodeJam/2017/Quals/BathroomStalls.py
+++ CodeJam/2017/Quals/BathroomStalls.py
@@ -18,38 +18,29 @@ class Stall():
             self.right, self.index)
 
 def bathroom_stalls(n, k):
-    # Initial the array of stalls
-    l_guard = Stall(True, 0)
-    r_guard = Stall(True, n + 1)
-
-    mid = int(n/2)
-
-    stalls_arr = [l_guard]
+    # Initialize the stalls array
+    stalls_arr = []
     for i in range(n):
-        stall = Stall(False, i + 1)
-        stall.left = i + 1
+        stall = Stall(False, i)
+        stall.left = i
         stall.right = n - i
         stalls_arr.append(stall)
         
-    stalls_arr.append(r_guard)
-
-    # for x in stalls_arr:
-    #     print(x)
-
     # Iterate each time for each person added
-    person_stalls = []
+    unoccupied_stalls = [x for x in stalls_arr]
     s = None
 
     for i in range(k):
         # First, find the optimal stalls
 
-        min_dis = max([x.min() for x in stalls_arr[1:n+1] if not x.occupied])
+        min_dis = max([x.min() for x in unoccupied_stalls if not x.occupied])
 
         # If there is only one such stall choose, it. Otherwise, choose 
         # the one for which s.max is maximal
         candidate_stalls =\
-            [x for x in stalls_arr[1:n+1] if x.min() == min_dis and not x.occupied]
-        max_dis = max([x.max() for x in candidate_stalls if not x.occupied])
+            [x for x in unoccupied_stalls if x.min() == min_dis]
+
+        max_dis = max([x.max() for x in candidate_stalls])
 
         if len(candidate_stalls) == 1:
             s = candidate_stalls[0]
@@ -63,7 +54,7 @@ def bathroom_stalls(n, k):
         index = s.index
 
         # Update every stall to the left of s
-        for i in range(1, index):
+        for i in range(index):
             t = stalls_arr[i]
 
             # End the loop if t is occupied
@@ -73,7 +64,7 @@ def bathroom_stalls(n, k):
             t.right = min(t.right, index - t.index)
 
         # Update every stall to the right of s
-        for i in range(index + 1, n + 1):
+        for i in range(index + 1, n):
             t = stalls_arr[i]
 
             # End the loop if t is occupied
@@ -81,7 +72,7 @@ def bathroom_stalls(n, k):
                 break
             t.left = min(t.left, t.index - index)
 
-        person_stalls.append(s)
+        unoccupied_stalls.remove(s)
 
     return (s.max()- 1, s.min() - 1)
 
@@ -90,23 +81,23 @@ def main():
         print('Error: program requires 3 arguments')
         return
 
-    try:
-        n = int(argv[1])
-        k = int(argv[2])
+    # try:
+    n = int(argv[1])
+    k = int(argv[2])
 
-        if n < 0 or k < 0:
-            print('Error : both n,k must be positive')
-            return
+    if n < 0 or k < 0:
+        print('Error : both n,k must be positive')
+        return
 
-        if (k > n):
-            print('Error : k must be less than n')
-            return
+    if (k > n):
+        print('Error : k must be less than n')
+        return
 
-        sol = bathroom_stalls(n, k)
-        print(sol)
+    sol = bathroom_stalls(n, k)
+    print(sol)
 
-    except:
-        print('Error : invalid input')
+    # except:
+    #     print('Error : invalid input')
 
 if __name__ == '__main__':
     main()
