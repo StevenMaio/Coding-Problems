7b86862a355c9dfae575ce18892e33f11da9db13
Steven Maio <Stevenmaio.321@gmail.com>
2018-07-02 10:23:41 -0400

    Added Problem 6

--- Daily Coding Problems/problem6/XOR_linked_list.c
+++ Daily Coding Problems/problem6/XOR_linked_list.c
@@ -1,4 +1,7 @@
-struct xor_linked_list {
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct xor_linked_list {
     int value;
     struct xor_linked_list *both;
 };
@@ -6,25 +9,34 @@ struct xor_linked_list {
 void add_helper(struct xor_linked_list *node, struct xor_linked_list *reference,
     int value)
 {
+    unsigned long both, ref, ptr;
+    both = (unsigned long) node->both;
+    ref = (unsigned long) reference;
+
+    ptr = both ^ ref;
     // if (both XOR reference == prev) is NULL, then we're at the end
-    if ((node->both ^ reference) == 0) {
-        // TODO: Create new node
-        struct xor_linked_list new_node = {value, node};
+    if (!ptr) {
+        // Create new node
+        struct xor_linked_list *new_node = 
+            (struct linked_list*) malloc(sizeof(struct xor_linked_list));
+
+        new_node->value = value;
+        new_node->both = node;
 
         // TODO: Set both of new_node to node, set both of node to reference ^ &new_node
-        node->both = node->both ^ &new_node;
+        ptr = (unsigned long) new_node ^ both;
+        node->both = (struct xor_linked_list*) ptr;
     }
     else
     {
-        // struct xor_linked_list *next = (struct xor_linked_list *) ((long) node->both) ^ ((long) reference);
-        struct xor_linked_list *next = node->both ^ reference;
-        // add_helper(node->both ^ reference, node, value);
+        // Cast ptr to a pointer to xor_linked_list and then call the helper
+        struct xor_linked_list *next = (struct xor_linked_list*) ptr;
         add_helper(next, node, value);
     }
 }
 
-void add(struct xor_linked_list head, int value) {
-    add_helper(&head, 0, value);
+void insert(struct xor_linked_list *head, int value) {
+    add_helper(head, 0, value);
 }
 
 int get_helper(struct xor_linked_list *node, struct xor_linked_list *reference, 
@@ -36,9 +48,36 @@ int get_helper(struct xor_linked_list *node, struct xor_linked_list *reference,
     }
 
     // Else
-    return get_helper(node->both ^ reference, node, index - 1);
+    unsigned long next_int =
+        (unsigned long) reference ^ (unsigned long) node->both;
+    struct xor_linked_list *next = (struct xor_linked_list*) next_int;
+    return get_helper(next, node, index - 1);
 }
 
-int get(struct xor_linked_list head, int index) {
-    return get_helper(&head, 0, index);
+int get(struct xor_linked_list *head, int index) {
+    return get_helper(head, 0, index);
+}
+
+// Lazy driver program
+int main(void)
+{
+    struct xor_linked_list *head;
+    int length, i, data;
+
+    length = 10;
+
+    head = (struct xor_linked_list*) malloc(sizeof(struct xor_linked_list));
+    head->value = 0;
+    head->both = NULL;
+
+    for (i = 1; i < length; i ++) {
+        insert(head, i);
+    }
+
+    for (i = 0; i < length; i++) {
+        int data = get(head, i);
+        printf("%d\n", data);
+    }
+
+    return 0;
 }
