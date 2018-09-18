804d6d3da0a110d035521abb37875073844ea483
Steven Maio <Stevenmaio.321@gmail.com>
2018-09-18 00:25:20 -0400

    Added Problem 180

---
+++ Daily Coding Problems/problem180/problem180.py
@@ -0,0 +1,39 @@
+def interleave(stack):
+	q = []
+
+
+	if len(stack) == 0:
+		return
+
+	# First, we'll empty the stack intothe q until it's empty
+	while True:
+		x = stack.pop(-1)
+
+		if len(stack) == 0:
+			s.append(x)
+			s.append()
+			break
+
+	# Check to see if the length of 
+	if len(q) == 0:
+		return stack
+
+	stack.append(q.pop(0))
+
+	while len(q) != 0:
+		z = q.pop(0)
+
+		q.append(z)
+		y = q.pop(0)
+
+		while not y is z:
+			a.append(y)
+
+		stack.append(z)
+
+		if len(q) == 0:
+			break
+		else:
+			stack.apend(q.pop(0))
+
+	return stack
